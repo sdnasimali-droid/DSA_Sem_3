6f0e3a0b1501ee02203f0ffa8d5a1e764aea8774
sdnasimali-droid <sdnasimali@gmail.com>
2026-09-08 20:55:08 +0530

    upload file

---
+++ mid_sem_assignment1.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int partition(int arr[],int low,int high){
+    int pivot = arr[high];
+    int i = (low - 1);
+    for(int j = low;j <high;j++){
+        if(arr[j] <= pivot){
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return (i + 1);
+}
+void quickSort(int arr[],int low,int high){
+    if(low < high){
+        int pi = partition(arr,low,high);
+        quickSort(arr,low,pi - 1);
+        quickSort(arr,pi + 1,high);
+    }
+}void bubbleSort( int array[], int size){
+    for (int step=0;step<1;step++){
+        for (int i=0;i<size-step-1;i++){
+            if (array[i] > array[i+1]){
+                int temp= array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+            }
+        }
+        printf("\nInternal array: \n");
+        for (int i = 0; i < size; i++) {
+            printf("%d  ", array[i]);
+        }
+        
+        quickSort(array,0,size-1);
+    }
+}
+int main(){
+    int data[]={4,7,1,9,8,5.2,3};
+    int size = sizeof(data)/sizeof(data[0]);
+    bubbleSort(data,size);
+    printf("\nSorted array :\n");
+    for(int i=0;i<size;i++){
+        printf("%d  ",data[i]);
+    }
+    return 0;
+}
